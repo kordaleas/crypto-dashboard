e90fd7d0b49bad1df23906be23aba489f75d2081
Alex Kord <kordaleas@hotmail.com>
2024-11-15 17:53:02 +0200

    fix: effect did not trigger

--- src/app/core/state/crypto.effects.ts
+++ src/app/core/state/crypto.effects.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
-import { map, mergeMap, catchError } from 'rxjs/operators';
+import { EMPTY, of } from 'rxjs';
+import { map, mergeMap, catchError, switchMap, tap } from 'rxjs/operators';
 import { CryptoService } from '../../core/services/crypto.service';
 import * as CryptoActions from './crypto.actions';
 
+
 @Injectable()
 export class CryptoEffects {
-  loadCryptos$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(CryptoActions.loadCryptos),
-      mergeMap(({ params }) =>
-        this.cryptoService.getCryptocurrencies(params).pipe(
-          map(data => CryptoActions.loadCryptosSuccess({ data })),
-          catchError(error => of(CryptoActions.loadCryptosFailure({ error })))
-        )
-      )
-    )
-  );
-
   constructor(
     private actions$: Actions,
     private cryptoService: CryptoService
-  ) {}
+  ) {
+  }
+
+
+  loadCryptos$ = createEffect(() => {
+    return of(null).pipe(
+      switchMap(() => this.actions$),
+      ofType(CryptoActions.loadCryptos),
+      switchMap(action => {
+        return this.cryptoService.getCryptocurrencies(action.params).pipe(
+          map(data => CryptoActions.loadCryptosSuccess({ data })),
+          catchError(error => of(CryptoActions.loadCryptosFailure({ error })))
+        )
+      })
+    );
+  });
 }
