131c243d45fc4c973347cb91d01afd3612e65c52
Alex Kord <kordaleas@hotmail.com>
2024-11-17 22:58:43 +0200

    feat: client side sorting

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,58 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { loadCryptos } from '../../core/state/crypto.actions';
 import { Observable } from 'rxjs';
 import { selectAllCryptos, selectError, selectLoading } from '../../core/state/crypto.selectors';
 import { Cryptocurrency } from '../../models/cryptocurrency.interface';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [CommonModule, MatTableModule, MatProgressSpinnerModule, MatPaginatorModule
-  ],
-  templateUrl: './dashboard.component.html'
+    selector: 'app-dashboard',
+    standalone: true,
+    imports: [CommonModule, MatTableModule, MatProgressSpinnerModule, MatPaginatorModule, MatSortModule
+    ],
+    templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
-  cryptos$: Observable<Cryptocurrency[]>;
-  loading$: Observable<boolean>;
-  error$: Observable<string | null>;
-  
-  displayedColumns = ['name', 'symbol', 'current_price'];
+    @ViewChild(MatSort) sort!: MatSort;
+    dataSource = new MatTableDataSource<Cryptocurrency>();
 
-  pageSize = 10;
-  pageSizeOptions = [5, 10, 25, 100];
+    cryptos$: Observable<Cryptocurrency[]>;
+    loading$: Observable<boolean>;
+    error$: Observable<string | null>;
 
-  constructor(private store: Store) {
-    this.cryptos$ = this.store.select(selectAllCryptos);
-    this.loading$ = this.store.select(selectLoading);
-    this.error$ = this.store.select(selectError);
-  }
+    displayedColumns = ['name', 'symbol', 'current_price'];
 
-  ngOnInit() {
-    this.store.dispatch(loadCryptos({ 
-      params: { 
-        vs_currency: 'usd',
-        order: 'market_cap_desc',
-        per_page: this.pageSize,
-        page: 1,
-        sparkline: false
-      } 
-    }));
-  }
+    pageSize = 10;
+    pageSizeOptions = [5, 10, 25, 100];
 
-  onPageChange(event: PageEvent) {
-    this.store.dispatch(loadCryptos({ 
-      params: { 
-        vs_currency: 'usd',
-        order: 'market_cap_desc',
-        per_page: event.pageSize,
-        page: event.pageIndex + 1,
-        sparkline: false
-      } 
-    }));
-  }
+    constructor(private store: Store) {
+        this.cryptos$ = this.store.select(selectAllCryptos);
+        this.loading$ = this.store.select(selectLoading);
+        this.error$ = this.store.select(selectError);
+
+        this.cryptos$.subscribe(data => {
+            this.dataSource.data = data;
+        });
+    }
+
+    ngOnInit() {
+        this.store.dispatch(loadCryptos({
+            params: {
+                vs_currency: 'usd',
+                order: 'market_cap_desc',
+                per_page: this.pageSize,
+                page: 1,
+                sparkline: false
+            }
+        }));
+    }
+
+    ngAfterViewInit() {
+        this.dataSource.sort = this.sort;
+    }
+
+    onPageChange(event: PageEvent) {
+        this.store.dispatch(loadCryptos({
+            params: {
+                vs_currency: 'usd',
+                order: 'market_cap_desc',
+                per_page: event.pageSize,
+                page: event.pageIndex + 1,
+                sparkline: false
+            }
+        }));
+    }
 }
